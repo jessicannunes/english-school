cd799fc5db1bda1a3edd309f232224b9d2eb4b26
Jessica <eng.diias@gmail.com>
2025-06-26 11:58:54 -0300

    Created Project

---
+++ src/main/java/com/jessicanuunes/englishschool/EnglishschoolApplication.java
@@ -0,0 +1,13 @@
+package com.jessicanuunes.englishschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jessicanuunes/englishschool/EnglishschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessicanuunes.englishschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
