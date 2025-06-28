0d8086814939af09e3ee6b4f5c0d948f76e2b21b
Jess Nunes <eng.diias@gmail.com>
2025-06-28 09:26:16 -0300

    Change the structure aplication.properties to run project

--- src/main/java/com/jessicanuunes/englishschool/Application.java
+++ src/main/java/com/jessicanuunes/englishschool/Application.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EnglishschoolApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EnglishschoolApplication.class, args);
+
+		SpringApplication.run(Application.class, args);
 	}
 
 }
